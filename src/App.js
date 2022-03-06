ea0fdc84086863dac928867ee972db8a7716ca0f
vk8958 <vk82851781@gmail.com>
2022-03-07 01:22:05 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import TableComponent from './components/TableComponent'
+import CarouselComponent from './components/CarouselComponent';
+import {Container,Button, ProgressBar } from 'react-bootstrap'
 
 function App() {
+  const [show,setShow]=useState(true)//true is for show default value
+  const [counter,setCounter]=useState(0)
+  const[show1,setShow1]=useState(false)
+  //const [show1,setShow1]=useState(false)
+  //useState is just 
+
+  //this.state={
+  //  show:true
+  //}
+//const arr=[2,3]
+//const []
+
+
+//Replicating componentDidMount using useEffect hook
+useEffect(()=>{
+  console.log('Component Mounted!')
+},[])
+
+//Replicating componentDidMount using useEffect hook
+useEffect(()=>{
+  console.log('Counter and state was updated')
+},[counter,show])
+//what i will pass [counter,show](dipendency array) if these any one will update then useEffect will run
+
+
+
+//Replicating componentWillMount using useEffect hook
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+      {show && <TableComponent />}
+        <Button variant='dark' onClick={()=>setShow(!show)}>{show?'Hide':'Show'}</Button><hr/>
+       <ProgressBar  now={10*counter}/><hr/>
+       <Button variant='dark' onClick={()=>setCounter(counter-1)} >Decrement</Button>
+        <Button variant='dark' onClick={()=>setCounter(counter+1)} >Increment</Button><br></br><br></br>
+       {show1&&<CarouselComponent/> } 
+       <Button variant='danger' onClick={()=>setShow1(!show1)}>GET CAROUSEL</Button>
+      </Container>
     </div>
   );
 }
---
+++ src/components/CarouselComponent.js
@@ -0,0 +1,54 @@
+
+import React from 'react' 
+import {Carousel} from 'react-bootstrap'
+
+
+
+
+
+
+
+
+function CarouselComponent() {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block  w-100"
+                    src="https://images.unsplash.com/photo-1638913662252-70efce1e60a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://media.istockphoto.com/photos/testing-software-picture-id1017296544?k=20&m=1017296544&s=612x612&w=0&h=I34gSez0j2HGciCztsZH09XiO142NLRmNIyFoJ-0U4M="
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720__480.jpg"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    )
+}
+
+export default CarouselComponent
